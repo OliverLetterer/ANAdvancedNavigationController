2d91a1535d46c1b8f87f775f771688cb37a3d68d
Oliver Letterer <oliver.letterer@googlemail.com>
2011-07-28 16:44:17 +0200

    initial release of sample application

---
+++ SampleApplication/SampleApplication/SampleApplicationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SampleApplicationAppDelegate.h
+//  SampleApplication
+//
+//  Created by Oliver Letterer on 28.07.11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleApplicationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleApplication/SampleApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleApplication
+//
+//  Created by Oliver Letterer on 28.07.11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SampleApplicationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SampleApplicationAppDelegate class]));
+    }
+}
